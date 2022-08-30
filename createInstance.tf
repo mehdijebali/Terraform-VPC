62d090c0d4a9cbd8086cbe7f0de654238c2eecdd
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2022-08-30 11:30:43 +0100

    createinstance.tf: set instance type

--- createInstance.tf
+++ createInstance.tf
@@ -6,7 +6,7 @@ resource "aws_key_pair" "levelup_key" {
 
 resource "aws_instance" "MyFirstInstnace" {
   ami           = var.AMI_ID
-  instance_type = "t2.micro"
+  instance_type = var.INSTANCE_TYPE
   key_name      = aws_key_pair.levelup_key.key_name
 
   vpc_security_group_ids = [aws_security_group.allow-levelup-ssh.id]
