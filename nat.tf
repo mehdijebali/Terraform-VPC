f4fa6d32564c2887435bf83c33dfa2449b271cbb
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2022-09-06 17:01:46 +0100

    Setup nat gateway

--- nat.tf
+++ nat.tf
@@ -1,28 +1,28 @@
 #Define External IP 
-resource "aws_eip" "levelup-nat" {
+resource "aws_eip" "eip-nat" {
   vpc = true
 }
 
-resource "aws_nat_gateway" "levelup-nat-gw" {
-  allocation_id = aws_eip.levelup-nat.id
-  subnet_id     = aws_subnet.levelupvpc-public-1.id
-  depends_on    = [aws_internet_gateway.levelup-gw]
+resource "aws_nat_gateway" "demo-nat-gw" {
+  allocation_id = aws_eip.eip-nat.id
+  subnet_id     = aws_subnet.public-subnet-B.id
+  depends_on    = [aws_internet_gateway.demo-gw]
 }
 
-resource "aws_route_table" "levelup-private" {
-  vpc_id = aws_vpc.levelupvpc.id
+resource "aws_route_table" "rt-private" {
+  vpc_id = aws_vpc.demo-vpc.id
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.levelup-nat-gw.id
+    nat_gateway_id = aws_nat_gateway.demo-nat-gw.id
   }
 
   tags = {
-    Name = "levelup-private"
+    Name = "rt-private"
   }
 }
 
 # route associations private
-resource "aws_route_table_association" "level-private-1-a" {
-  subnet_id      = aws_subnet.levelupvpc-private-1.id
-  route_table_id = aws_route_table.levelup-private.id
+resource "aws_route_table_association" "private-b" {
+  subnet_id      = aws_subnet.private-subnet-B
+  route_table_id = aws_route_table.rt-private.id
 }
