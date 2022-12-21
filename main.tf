1233dc1117fdb752cc46aa36a07a076018c002f0
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2022-12-21 09:53:40 +0100

    Configure network module variables

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
 provider "aws" {
   region     = var.AWS_REGION
 }
+
+module "network" {
+    source      = "./modules/network"
+
+    VPC_NAME = var.VPC_NAME
+    VPC_CIDR_BLOCK  = var.VPC_CIDR_BLOCK
+    GW_NAME = var.GW_NAME
+    PUBLIC_RT_NAME = var.PUBLIC_RT_NAME
+    PRIVATE_RT_NAME = var.PRIVATE_RT_NAME
+    AVAILABILITY_ZONES = var.AVAILABILITY_ZONES
+    SUBNET_NAMES = var.SUBNET_NAMES
+    SUBNET_IPS = var.SUBNET_IPS
+}
