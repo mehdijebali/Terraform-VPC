7fb1cc4838517ab514f3a00f56065c0d0c80b4c8
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2022-12-21 11:42:07 +0100

    remove unnecessary variables

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -2,7 +2,6 @@ variable "VPC_NAME" {}
 variable "VPC_CIDR_BLOCK" {}
 variable "GW_NAME" {}
 variable "PUBLIC_RT_NAME" {}
-variable "PRIVATE_RT_NAME" {}
 
 variable "AVAILABILITY_ZONES" {
    type = list
