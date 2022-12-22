41a4b30e52501e8a86c1d2952e2956629c2e04f2
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2022-12-22 14:25:51 +0100

    Configure instance module

--- main.tf
+++ main.tf
@@ -14,3 +14,17 @@ module "network" {
     SUBNET_NAMES = var.SUBNET_NAMES
     SUBNET_IPS = var.SUBNET_IPS
 }
+
+module "instance" {
+  source = "./modules/instance"
+
+  KEY_NAME = var.KEY_NAME
+  PATH_TO_PUBLIC_KEY = var.PATH_TO_PUBLIC_KEY
+  SG_VPC_ID = module.network.vpc_id
+  SG_NAME = var.SG_NAME
+  SG_DESCRIPTION = var.SG_DESCRIPTION
+  AMI_ID = var.AMI_ID
+  INSTANCE_TYPE = var.INSTANCE_TYPE
+  INSTANCE_SUBNET_ID = module.network.public_subnet_A_id
+  INSTANCE_NAME = var.INSTANCE_NAME
+}
