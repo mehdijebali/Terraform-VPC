8be6e53654414b00b0f5f2b0be29786f7a83f923
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2022-12-22 14:06:48 +0100

    Insert SG resource into instance module

--- createInstance.tf
+++ createInstance.tf
@@ -4,6 +4,31 @@ resource "aws_key_pair" "levelup_key" {
     public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
 
+#Security Group for levelupvpc
+resource "aws_security_group" "allow-levelup-ssh" {
+  vpc_id      = module.network.vpc_id
+  name        = var.SG_NAME
+  description = var.SG_DESCRIPTION
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  tags = {
+    Name = "${var.SG_NAME}"
+  }
+}
+
 resource "aws_instance" "MyFirstInstnace" {
   ami           = var.AMI_ID
   instance_type = var.INSTANCE_TYPE
